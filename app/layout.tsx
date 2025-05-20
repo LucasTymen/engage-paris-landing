d0cd31387ea948f4a06a15e907638b4cdf54e611
Lucas Tymen <lucas.tymen@gmail.com>
2025-05-20 11:07:42 +0200

    adding the google ads tracker_5

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
@@ -19,20 +18,19 @@ export default function RootLayout({
   return (
     <html lang="fr">
       <head>
-        {/* ✅ Google Tag Manager (code original non modifié) */}
-        <Script id="gtm-init" strategy="beforeInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-PDVKS78C');
-          `}
-        </Script>
+        {/* ✅ Google Tag Manager – HEAD (injected via script tag safely) */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PDVKS78C');`,
+          }}
+        />
       </head>
-
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        {/* ✅ Google Tag Manager noscript fallback */}
+        {/* ✅ Google Tag Manager – NOSCRIPT fallback */}
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-PDVKS78C"
